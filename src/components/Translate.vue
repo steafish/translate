e8de1a592d816faad7f0608f86632af9448048af
steafish@gmail.com <torstein@PC.localdomain>
2020-09-27 19:20:51 +0200

    First commit

---
+++ src/components/Translate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="translate">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    pid: {
+      type: String,
+      required: false
+    },
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.translate {
+}
+</style>
